5e280dcd87560fcef482c5f95389470aee4f714c
Oğuzhan Ergün <Oguzh4n15@hotmail.com>
2023-09-13 19:29:28 +0300

    CameraScriptCreated

---
+++ Assets/Camera/FollowPlayer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform mainPlayer;
+    [SerializeField] float followSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+        if (mainPlayer != null)
+        {
+            Vector3 targetPosition = new Vector3(transform.position.x, mainPlayer.position.y + 5, mainPlayer.position.z);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+        }
+
+    }
+}
